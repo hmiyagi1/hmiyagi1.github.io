4b30e627cc3f93e818a355377d2022ee24fa7177
hmiyagi <harunori.miyagi@gmail.com>
2020-01-13 19:24:55 -0700

    upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+new Date().getFullYear()
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+	
+
+var date = document.lastModified;
+         document.write("Last Updated: "+ date);
