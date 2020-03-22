c70c0203132a11d01148fb6b3d4ce90410abe8e2
Harunori Miyagi <harunori.miyagi@gmail.com>
2020-03-21 21:03:34 -0600

    Add files via upload

---
+++ fishhaven/js/5dayforecast.js
@@ -0,0 +1,67 @@
+const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&APPID=fa32a8a242aa5025fb20b3e30f41ee01";
+
+fetch(apiForecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+	console.log(jsObject);
+	
+	const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+	for (let i = 0; i < jsObject.list.length; i++ ){
+		if (new Date(jsObject.list[i].dt_txt).getHours() == 18) {
+
+			var forecastAPI = jsObject.list[i];
+			
+			let div1 = document.createElement('div');
+			let div2 = document.createElement('div');
+			let head = document.createElement('strong');
+			let div3 = document.createElement('div');
+			let image = document.createElement('img');
+			let temp = document.createElement('p');
+
+			const imageidentifier = forecastAPI.weather[0].main;
+			if (imageidentifier == "Clear"){
+				var imgSource = "assets/sunny.png";
+			} 
+			else if (imageidentifier == "Clouds"){
+				var imgSource = "assets/cloud.png";
+			}
+			else if (imageidentifier == "Snow"){
+				var imgSource = "assets/snow.png";
+			}
+			else if (imageidentifier == "Rain" || imageidentifier == "Drizzle"){
+				var imgSource = "assets/rain.png";
+			}
+			else if (imageidentifier == "Thunderstorm"){
+				var imgSource = "assets/thunderstorm.png";
+			}
+			else {
+				var imgSource = "assets/mist.png"
+			}
+
+
+			head.textContent = dayOfWeek[new Date(forecastAPI.dt_txt).getDay()];
+			image.setAttribute('src', imgSource);
+			image.setAttribute('alt', forecastAPI.weather[0].description);
+			temp.innerHTML = forecastAPI.main.temp.toFixed(1) + " &#8457;";
+
+			div1.appendChild(div2);
+			div2.classList.add("Day");
+			div2.appendChild(head);
+			div3.classList.add("DayContent");
+			div1.appendChild(div3);
+			div3.appendChild(image);
+			div3.appendChild(temp);
+
+			document.querySelector('div.forecastTable').appendChild(div1);
+
+		}
+	}
+  });
+  
+
+
+
+
+
+
